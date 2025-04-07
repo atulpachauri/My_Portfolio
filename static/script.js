59023beadc17ec5e702b0f39f9785f588e31e2a9
Mariya <mariyasha432@protonmail.com>
2025-04-07 09:05:01 -0700

    removing unnescessary files

--- static/script.js
+++ static/script.js
@@ -1,8 +1,1 @@
-function toggleGreeting() {
-    let heading = document.getElementById("greeting");
-    if (heading.innerText === "Hello World") {
-        heading.innerText = "Goodbye World";
-    } else {
-        heading.innerText = "Hello World";
-    }
-}
+// to be updated
